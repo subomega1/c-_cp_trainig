7c892f10f1ace514ed4e0a6a784d229742b7aee9
subomega1 <ahmedkhalil.sfar@etudiant-fst.utm.tn>
2024-05-28 10:58:57 +0100

    done func

--- PROBLEMS/1000prob.cc
+++ PROBLEMS/1000prob.cc
@@ -1,22 +1,54 @@
-
+#include<vector>
+#include<algorithm>
+int supp(int t[],int n)
+{
+  int aux=t[0];
+  t[0]=t[n-1];
+  t[n-1]=aux;
+  n--;
+  if (n==0)
+  t=nullptr;
+  return 0;
+}
 
 
 int fusion(int t1[],int t2[],int n)
 {
   int t3[2*n];
-  int i=0,j=0,k=0;
-  while (i<n)
+  int k=2*n-1,n1=n;
+  
+  while (t1!=nullptr)
   {
-    while (j<n)
+    while (t2!=nullptr)
     {
-      t3[k]=t1[j];
-      k++;
-      j++;
+      t3[k]=t2[0];
+      k--;
+      supp(t2,n);
+      
     }
-    t3[k]=t2[i];
-    k++;
-    i++;
+    t3[k]=t1[0];
+    k--;
+    supp(t1,n1);
   
   }
   
+}
+int nb_oc(int t[],int n, int x)
+{
+  int nboc=0;
+  for (int i=0;i<n;i++)
+  {
+    if (t[i]==x)
+    nboc++;
+  }
+  return nboc;
+}
+int lenth_sub(int t[], int n)
+{
+  std::vector<int> nb_list;
+  for (int i = 0; i < n; i++)
+  {
+    nb_list.push_back(nb_oc(t, n, t[i]));
+  }
+  return *std::max_element(nb_list.begin(), nb_list.end());
 }
